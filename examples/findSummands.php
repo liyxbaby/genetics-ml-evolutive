9bd01c0811e01752d9ddf3a8bc7c164616d28c56
liyxbaby <liyxbaby@proton.me>
2023-10-06 14:31:23 +0000

    Added Bulgarian translation

--- examples/findSummands.php
+++ examples/findSummands.php
@@ -1,4 +1,4 @@
 <?php
 
 	/**
-	 * Example of Evolutive https://github.com/tin-cat/evolutive, A l
+	 * Example of Evolutive https://github.com/tin-cat/evolutive, A library to learn about simple evolutive, genetics-based machine learning alg
