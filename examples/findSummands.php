6cd5e2dd4c1442683b3da4688bde0f97954c13cc
liyxbaby <liyxbaby@proton.me>
2023-10-08 11:32:33 +0000

    Test updates.

--- examples/findSummands.php
+++ examples/findSummands.php
@@ -1,4 +1,5 @@
 <?php
 
 	/**
-	 * Example of Evolutive https://github.com/tin-cat/evolutive, A library to learn about simple evolutive, genetics-based machine learning alg
+	 * Example of Evolutive https://github.com/tin-cat/evolutive, A library to learn about simple evolutive, genetics-based machine learning algorithms and object-oriented programming by Tin.cat
+	 * Using an evol
