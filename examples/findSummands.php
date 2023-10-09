ab53f61a978d07e529adc2d92d5f7fa094ba136e
liyxbaby <liyxbaby@proton.me>
2023-10-09 18:17:14 +0000

    Fixed filters

--- examples/findSummands.php
+++ examples/findSummands.php
@@ -2,4 +2,7 @@
 
 	/**
 	 * Example of Evolutive https://github.com/tin-cat/evolutive, A library to learn about simple evolutive, genetics-based machine learning algorithms and object-oriented programming by Tin.cat
-	 * Using an evolutive, genetics-based machine learning algorithm to find the two su
+	 * Using an evolutive, genetics-based machine learning algorithm to find the two sumands that give a specific result.
+	 */
+
+	header("Content-type: text/plain; ch
